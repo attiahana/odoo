0cbaca6f55865f11acd8c6e5500c14dead6f913e
hana@hana-pc <hana@hana-pc.com>
2023-12-28 19:50:51 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Hospital Managment',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'By Hana',
+    'summary': 'Hospital managment system',
+    'sequance': -100,
+    'description': """Hospital managment system""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'asset': {},
+}
